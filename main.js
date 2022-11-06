85c2e95170f9d247722b70d1c606e2f51c2dfe33
Christian <nesd4rkfocus@gmail.com>
2022-11-06 13:41:13 -0300

    Project which Vite

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "./style.css"
+// import "./style.css"
 
 // var theme = prompt("Escolha a cor: {yellow}, {blue}, {green}")
 
@@ -39,33 +39,44 @@ function createCard(date, weekday, games) {
   delay += 0.2
   return `
         <div class="cards" style="animation-delay: ${delay}s;">
-          <div class="button">
-            <div class="date">${date}</div>
-            <div class="weekday">${weekday}</div>
-          </div>
+        <nav>
+        <img id="left-arrow" src="./public/l-arrow.svg" alt="Jogo anterior"/>
+        <div class="button">
+          <div class="date">${date}</div>
+          <div class="weekday">${weekday}</div>
+        </div>
+        <img id="right-arrow" src="./public/r-arrow.svg" alt="Próximo jogo" />
+        </nav>
+
           ${games}
         </div>
         `
 }
 
-document.querySelector("main").innerHTML =
+let cards = [
+  createCard("20/11", "Domingo", createGame("qatar", "13:00", "ecuador")),
+  createCard(
+    "21/11",
+    "Segunda",
+    createGame("england", "10:00", "ecuador") +
+      createGame("senegal", "13:00", "netherlands") +
+      createGame("usa", "16:00", "wales")
+  ),
   createCard(
     "22/11",
     "Terça",
     createGame("br", "08:00", "serbia") + createGame("br", "08:00", "serbia")
-  ) +
+  ),
   createCard(
     "22/11",
     "Terça",
     createGame("br", "08:00", "serbia") + createGame("br", "08:00", "serbia")
-  ) +
-  createCard(
-    "22/11",
-    "Terça",
-    createGame("br", "08:00", "serbia") + createGame("br", "08:00", "serbia")
-  ) +
-  createCard(
-    "22/11",
-    "Terça",
-    createGame("br", "08:00", "serbia") + createGame("br", "08:00", "serbia")
-  )
+  ),
+]
+
+let position = 3
+
+document.querySelector("main").innerHTML = cards[position]
+// function back(postion) {
+//   postion
+// }
